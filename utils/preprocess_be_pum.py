de4338b456c7a2004eecca12a1b637169da1cc08
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-03-22 01:30:49 -0700

    fix remove unnecessary char

--- utils/preprocess_be_pum.py
+++ utils/preprocess_be_pum.py
@@ -1,13 +1,18 @@
 import os
-
+import re
 log_path = "data/logs"
 asm_cfg_path = "data/asm_cfg"
 
 
+def remove_unnecessary_char(s):
+    return  re.sub(r"[\n\t\s]*", "", s)
+
 def get_end_of_unpacking(name):
     file = os.path.join(log_path, "Log-" + name + ".log")
+    if not os.path.exists(file):
+        return None
     with open(file, "r") as f:
-        last_line = f[-1]
+        last_line = f.readlines()[-1]
         if "Packed Identified" in last_line:
             words = last_line.split(" ")
             for word in words:
@@ -19,12 +24,20 @@ def get_end_of_unpacking(name):
 
 def get_OEP_of_UPX(name):
     file = os.path.join(asm_cfg_path, name + "_code.asm")
+    if not os.path.exists(file):
+        return None, None
     with open(file, "r") as f:
         traces = []
         for line in f:
-            address, instruction = line.split(":")
+            # print(line)
+            # print(line.split(":"))
+            g = line.find(":")
+            address = line[:g]
+            instruction = remove_unnecessary_char(line[g + 1:])
+            # address, instruction = line.split(":")
             traces.append((address, instruction))
         for idx, trace in enumerate(traces):
+            print(trace[1])
             if trace[1] == "popa":
                 return traces[idx + 6][0], traces[idx + 7][0]
     return None, None
@@ -38,11 +51,15 @@ def update_information_UPX(packed_file_path):
     for name in packed_file:
         previous_OEP, OEP = get_OEP_of_UPX(name)
         if OEP is not None:
+            if not (name in information):
+                information[name] = {}
             information[name]["previous_OEP"] = previous_OEP
             information[name]["OEP"] = OEP
 
     for name in packed_file:
         end_unpacking_address = get_end_of_unpacking(name)
         if end_unpacking_address is not None:
+            if not (name in information):
+                information[name] = {}
             information[name]["end_unpacking"] = end_unpacking_address
     return information
--- visualize_points.py
+++ visualize_points.py
@@ -5,7 +5,8 @@ packed_list_path = "data/packed_files.txt"
 
 def main():
     information = update_information_UPX(packed_list_path)
-
+    print(information)
+    print(len(information))
 
 if __name__ == '__main__':
     main()
