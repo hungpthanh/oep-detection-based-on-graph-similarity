40f50c4076d27dbc8f10e2030b5507f999483f3a
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-03-22 02:19:55 -0700

    visualize data points

--- visualize_points.py
+++ visualize_points.py
@@ -1,4 +1,5 @@
 from utils.preprocess_be_pum import update_information_UPX
+import matplotlib.pyplot as plt
 
 packed_list_path = "data/packed_files.txt"
 
@@ -7,6 +8,28 @@ def main():
     information = update_information_UPX(packed_list_path)
     print(information)
     print(len(information))
+
+    with open(packed_list_path, "r") as f:
+        packed_file = [line.strip() for line in f]
+    x = []
+    y = []
+    for name in packed_file:
+        if not (name in information):
+            continue
+        print(name)
+        print(information[name])
+        if not ("end_unpacking" in information[name]):
+            continue
+        if not ("previous_OEP" in information[name]):
+            continue
+        x.append(int(information[name]["end_unpacking"], base=16))
+        y.append(int(information[name]["previous_OEP"], base=16))
+    # x = [1, 2, 3, 4, 5, 6, 7, 8]
+    # y = [2, 3, 1, 3, 1, 4, 2, 3]
+
+    plt.scatter(x, y)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
