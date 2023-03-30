c4a71d6edb9bd54c64543da40b912ea8c1937b25
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-03-30 17:34:19 +0900

    create BPCFG

--- utils/pygraphviz_demmo.py
+++ utils/pygraphviz_demmo.py
@@ -1,4 +1,5 @@
 import pygraphviz as pgv
+
 visited = []
 name_file = "fsg_Cacheset.exe_model.dot"
 name_file = "fsg_accesschk.exe_model.dot"
@@ -17,8 +18,11 @@ class Node:
         self.address = address
         self.opcode = opcode
 
+
 adj = {}
 label = {}
+
+
 def get_node_information(s):
     if not s.startswith('a0x'):
         address = s
@@ -29,9 +33,11 @@ def get_node_information(s):
 
     return address, opcode
 
+
 def get_edge_informaton(p):
     source, target = p
     source, target = get_node_information(source), get_node_information(target)
+
 
 start_node = -1
 
@@ -49,6 +55,7 @@ for edge in G.edges():
     if not (target[0] in adj[source[0]]):
         adj[source[0]].append(target[0])
 
+
 def compare(item1, item2):
     if label[item1] < label[item2]:
         return -1
@@ -57,9 +64,11 @@ def compare(item1, item2):
     else:
         return 0
 
+
 for key, value in adj.items():
     # value.sort(key=compare)
     value = sorted(value, key=lambda x: label[x])
+
 
 # print(adj)
 
@@ -70,8 +79,37 @@ def dfs(u):
         if not v in visited:
             dfs(v)
 
+
 dfs(start_node)
 print(visited)
 with open(name_file + ".asm", "w") as f:
     for node in visited:
         f.writelines("{}: {}\n".format(node, label[node]))
+
+
+class BPCFG():
+    def __init__(self, dot_file):
+        self.G = pgv.AGraph(dot_file)
+        self.start_node = -1
+        self.adj = {}
+        self.label = {}
+        for idx, node in enumerate(G.nodes()):
+            address, opcode = get_node_information(node)
+            if not address in adj:
+                adj[address] = []
+                label[address] = opcode
+            if idx == 0:
+                self.start_node = address
+
+        for edge in G.edges():
+            source, target = edge
+            source, target = get_node_information(source), get_node_information(target)
+            if not (target[0] in adj[source[0]]):
+                self.adj[source[0]].append(target[0])
+
+    def get_incoming_node(self, node_address):
+        results = []
+        for key, value in self.adj.items():
+            if node_address in value:
+                results.append(key)
+        return results
