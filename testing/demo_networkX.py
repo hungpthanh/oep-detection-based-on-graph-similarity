832e3ffa536f4391d0d4b834f136fd775e17f8b5
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-06 22:57:32 +0900

    refactor graph with relabel and set attribution

--- testing/demo_networkX.py
+++ testing/demo_networkX.py
@@ -1,26 +1,52 @@
+import os.path
+
 import networkx as nx
 from networkx.drawing.nx_agraph import write_dot, read_dot
 
+from utils.oep_utils import get_oep_dataset
+from utils.preprocess_be_pum import update_information_FSG
 from utils.pygraphviz_demmo import get_node_information
 
-G = nx.DiGraph(read_dot(path="data/asm_cfg/upx_accesschk.exe_model.dot"))
+oep_dictionary = get_oep_dataset()
+packed_list_path = "data/packed_files_FSG.txt"
+asm_cfg_path = "data/asm_cfg"
 
-print(type(G))
+information = update_information_FSG(packed_list_path)
 
+with open(packed_list_path, "r") as f:
+    packed_file = [line.strip() for line in f]
 
-print(G.nodes())
-print(G.edges())
-
-# paths = nx.all_simple_paths(G, "a0x00540000movl_0x4001d0UINT32_ebx", "a0x0040c429call_0x414ad4")
-# for path in paths:
-#     print(path)
-def get_opcode(s):
-    address, _ = get_node_information(s)
-    return _.split("_")[0]
+G = {}
+paths = {}
 
 
-paths = list(nx.shortest_simple_paths(G, "a0x004c4980pusha_", "a0x0040c429call_0x414ad4"))
-for path in paths:
-    print(len(path))
-    path = [get_opcode(s) for s in path]
-    print(path)
+def relabel_graph(G):
+    attribution_mapping = {}
+    label_mapping = {}
+    for node in G.nodes:
+        address, opcode = get_node_information(node)
+        attribution_mapping[address] = {"opcode": opcode}
+        label_mapping[node] = address
+    nG = nx.relabel_nodes(G, label_mapping)
+    nx.set_node_attributes(nG, attribution_mapping)
+    return nG
+
+
+for name in packed_file:
+    if not (name in information):
+        continue
+    # print(name)
+    # print(information[name])
+    if not ("end_unpacking" in information[name]):
+        continue
+    if not ("previous_OEP" in information[name]):
+        continue
+    G[name] = nx.DiGraph(read_dot(path=os.path.join(asm_cfg_path, name + "_model.dot")))
+    G[name] = relabel_graph(G[name])
+
+    print("name = {}".format(name))
+    source = list(G[name].nodes)[0]
+    target = information[name]["previous_OEP"]
+    paths[name] = list(nx.shortest_simple_paths(G[name], source, target))
+    for path in paths[name]:
+        print(path)
