f9765657cf20382abecb7c8972ec2bad7cb7bd1c
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-10 14:59:18 +0900

    add code for aspack

--- utils/preprocess_be_pum.py
+++ utils/preprocess_be_pum.py
@@ -11,8 +11,8 @@ asm_cfg_path = "data/asm_cfg"
 def remove_unnecessary_char(s):
     return re.sub(r"[\n\t\s]*", "", s)
 
-def get_end_of_unpacking(name):
-    file = os.path.join(log_path, "Log-" + name + ".log")
+def get_end_of_unpacking(name, packer_name):
+    file = os.path.join(log_path, packer_name, "Log-" + name + ".log")
     if not os.path.exists(file):
         return None
     with open(file, "r") as f:
@@ -76,7 +76,7 @@ def update_information_FSG(packed_file_path):
         packed_file = [line.strip() for line in f]
     oep_dictionary = get_oep_dataset()
     for idx, name in enumerate(packed_file):
-        dot_file = os.path.join(asm_cfg_path, name + "_model.dot")
+        dot_file = os.path.join(asm_cfg_path, "fsg", name + "_model.dot")
         if not os.path.exists(dot_file):
             continue
         print("name file: {}".format(name))
@@ -108,7 +108,53 @@ def update_information_FSG(packed_file_path):
     for name in packed_file:
         if not name in information:
             continue
-        end_unpacking_address = get_end_of_unpacking(name)
+        end_unpacking_address = get_end_of_unpacking(name, "fsg")
+        if end_unpacking_address is not None:
+            if not (name in information):
+                information[name] = {}
+            information[name]["end_unpacking"] = end_unpacking_address
+    print(len(information))
+    return information
+
+def update_information_ASPACK(packed_file_path):
+    information = {}
+    with open(packed_file_path, "r") as f:
+        packed_file = [line.strip() for line in f]
+    oep_dictionary = get_oep_dataset()
+    for idx, name in enumerate(packed_file):
+        dot_file = os.path.join(asm_cfg_path, "aspack", name + "_model.dot")
+        if not os.path.exists(dot_file):
+            continue
+        print("name file: {}".format(name))
+        try:
+            cfg = BPCFG(dot_file)
+        except Exception as e:
+            print(e)
+            continue
+        # name = name[4:]
+
+        if not name[7:] in oep_dictionary:
+            print("Dont have the OEP of {}".format(name))
+            continue
+        OEP = oep_dictionary[name[7:]]
+        previous_OEP = cfg.get_incoming_node(OEP)
+        print("OEP = {}".format(OEP))
+        print("previous OEP = {}".format(previous_OEP))
+        if len(previous_OEP) > 1:
+            print("multiple incoming node to OEP")
+        if OEP is not None and len(previous_OEP) > 0:
+            if not (name in information):
+                information[name] = {}
+            information[name]["previous_OEP"] = previous_OEP[0]
+            information[name]["OEP"] = OEP
+        #
+        # break
+    print(information)
+
+    for name in packed_file:
+        if not name in information:
+            continue
+        end_unpacking_address = get_end_of_unpacking(name, "aspack")
         if end_unpacking_address is not None:
             if not (name in information):
                 information[name] = {}
