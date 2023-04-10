b51b04e7da926b43fae70a9dae5e0f00b4811537
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-10 14:58:51 +0900

    add some demo code for matching graph

--- utils/graphviz_demo.py
+++ utils/graphviz_demo.py
@@ -2,7 +2,8 @@ import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
 
 from graphviz import Source
-path = 'utils/aspack_ADInsight.exe_model.dot'
+# path = 'utils/aspack_ADInsight.exe_model.dot'
+path = 'data/asm_cfg/aspack/aspack_accesschk.exe_model.dot'
 s = Source.from_file(path)
 s.view()
 
