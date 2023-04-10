9c0141061e8a4c053c5f0dd50d1e8f5ce8b722ae
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-10 15:00:15 +0900

    refactor

--- visualize_points.py
+++ visualize_points.py
@@ -120,57 +120,7 @@ def main(packer_name):
     # plt.show()
 
 
-def linear_regression():
-    # Code source: Jaques Grobler
-    # License: BSD 3 clause
 
-    import matplotlib.pyplot as plt
-    import numpy as np
-    from sklearn import datasets, linear_model
-    from sklearn.metrics import mean_squared_error, r2_score
-
-    # Load the diabetes dataset
-    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-    diabetes_X, diabetes_y = get_X_y()
-    diabetes_X = np.asarray(diabetes_X).reshape((-1, 1))
-    diabetes_y = np.asarray(diabetes_y).reshape((-1, 1)).ravel()
-    print(diabetes_X.shape)
-    # Use only one feature
-    # diabetes_X = diabetes_X[:, np.newaxis, 2]
-
-    print(diabetes_X.shape)
-    # Split the data into training/testing sets
-    diabetes_X_train = diabetes_X[:]
-    diabetes_X_test = diabetes_X[-20:]
-
-    # Split the targets into training/testing sets
-    diabetes_y_train = diabetes_y[:]
-    diabetes_y_test = diabetes_y[-20:]
-
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
-
-    # Train the model using the training sets
-    regr.fit(diabetes_X_train, diabetes_y_train)
-
-    # Make predictions using the testing set
-    diabetes_y_pred = regr.predict(diabetes_X_test)
-
-    # The coefficients
-    print("Coefficients: \n", regr.coef_)
-    # The mean squared error
-    print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-    # The coefficient of determination: 1 is perfect prediction
-    print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
-
-    # Plot outputs
-    plt.scatter(diabetes_X_train, diabetes_y_train, color="green")
-    plt.plot(diabetes_X_train, diabetes_y_train, color="blue", linewidth=1)
-
-    plt.xticks(())
-    plt.yticks(())
-
-    plt.show()
 
 
 def bar_chart(packer_name):
@@ -213,5 +163,4 @@ def bar_chart(packer_name):
 
 if __name__ == '__main__':
     # main("aspack")
-    # linear_regression()
     bar_chart("aspack")
