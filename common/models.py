e7862a20ceb12e5c342a23b263eb7455569a7d98
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-11 11:15:02 +0900

    add remove back edge

--- common/models.py
+++ common/models.py
@@ -1,4 +1,8 @@
+import time
+from copy import deepcopy
+
 import networkx as nx
+from matplotlib import pyplot as plt
 from networkx.drawing.nx_agraph import read_dot
 
 from utils.graph_utils import relabel_graph
@@ -27,7 +31,37 @@ class CFG():
             if not (source in self.rev_adj[target]):
                 self.rev_adj[target].append(source)
 
+    def reset(self):
+        self.nx_graph = nx.DiGraph()
+        self.nodes = []
+        self.start_node = None
+        self.adj = {}
+        self.rev_adj = {}
+
+    def add_node(self, address, opcode):
+        if not (address in self.nodes):
+            self.nx_graph.add_node(address, opcode=opcode)
+            self.nodes.append(address)
+
+    def get_node_attribute(self, address):
+        return self.nx_graph.nodes[address]
+
+    def add_edge(self, address1, opcode1, address2, opcode2):
+        if not (self.nx_graph.has_edge(address1, address2)):
+            if not self.nx_graph.has_node(address1):
+                self.add_node(address1, opcode1)
+            if not self.nx_graph.has_node(address2):
+                self.add_node(address2, opcode2)
+            self.nx_graph.add_edge(address1, address2)
+            self.adj[address1].append(address2)
+            self.rev_adj[address2].append(address1)
+
+
+
 
 
 if __name__ == '__main__':
-    cfg = CFG(dot_file="data/asm_cfg/upx/upx_accesschk.exe_model.dot")
+    cfg = relabel_graph(nx.DiGraph(read_dot(path="data/asm_cfg/upx/upx_accesschk.exe_model.dot")))
+    new_cfg = remove_back_edege(cfg)
+    print(new_cfg.nodes)
+    nx.nx_agraph.write_dot(new_cfg, "my_graph.dot")
