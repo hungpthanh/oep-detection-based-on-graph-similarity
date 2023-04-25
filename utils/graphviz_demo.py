8aab74726f1a6f9c834d4c84e7b446f3029da6e4
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-25 21:54:29 +0900

    render pdf for graph with multiple file

--- utils/graphviz_demo.py
+++ utils/graphviz_demo.py
@@ -1,8 +1,22 @@
+import glob
 import os
+
+from tqdm import tqdm
+
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
 
 from graphviz import Source
 # path = 'utils/aspack_ADInsight.exe_model.dot'
-path = 'data/asm_cfg/aspack/aspack_accesschk.exe_model.dot'
-s = Source.from_file(path)
-s.view()
+# path = 'utils/colored_aspack_accesschk.exe_model.dot'
+# s = Source.from_file(path)
+# s.view()
+
+def generate_graph():
+    paths = glob.glob("logs/log_graph_color/*.*")
+    for path in tqdm(paths):
+        s = Source.from_file(path)
+        # s.save(directory="logs/log_graph_color")
+        s.render()
+
+if __name__ == '__main__':
+    generate_graph()
