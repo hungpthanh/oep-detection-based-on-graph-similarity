5e4377f0f777722d2ff1ebd04e688065d9e498b0
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-04-26 22:14:02 +0900

    add mode generate single file

--- utils/graphviz_demo.py
+++ utils/graphviz_demo.py
@@ -9,13 +9,15 @@ os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
 from graphviz import Source
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--mode', default="evaluation", type=str)
+parser.add_argument('--mode', default="single", type=str)
 parser.add_argument("--packers", nargs="+", default=["upx"])
 # parser.add_argument('--packer_name', default="upx", type=str)
-# parser.add_argument('--file_name', default="accesschk.exe", type=str)
+parser.add_argument('--file_name', default="accesschk.exe", type=str)
 
 # Get the arguments
 args = parser.parse_args()
+
+
 # path = 'utils/aspack_ADInsight.exe_model.dot'
 # path = 'utils/colored_aspack_accesschk.exe_model.dot'
 # s = Source.from_file(path)
@@ -33,5 +35,15 @@ def generate_graph():
                 s.render()
 
 
+def generate_single():
+    path = os.path.join("logs/log_graph_color/", "colored_{}_{}_model.dot".format(args.packers[0], args.file_name))
+    s = Source.from_file(path)
+    # s.save(directory="logs/log_graph_color")
+    s.render()
+
+
 if __name__ == '__main__':
-    generate_graph()
+    if args.mode == "all":
+        generate_graph()
+    else:
+        generate_single()
