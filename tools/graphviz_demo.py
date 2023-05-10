33c80d2ebe6f54d18aa9b6761e91f7a6871e4ef1
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-05-10 17:28:14 +0900

    update graph viz

--- tools/graphviz_demo.py
+++ tools/graphviz_demo.py
@@ -1,9 +1,10 @@
 import argparse
 import glob
 import os
+import sys
 
 from tqdm import tqdm
-
+sys.path.append('.')
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
 
 from graphviz import Source
@@ -13,7 +14,7 @@ parser.add_argument('--mode', default="single", type=str)
 parser.add_argument("--packers", nargs="+", default=["upx"])
 # parser.add_argument('--packer_name', default="upx", type=str)
 parser.add_argument('--file_name', default="accesschk.exe", type=str)
-
+parser.add_argument('--data_folder', default=".", type=str)
 # Get the arguments
 args = parser.parse_args()
 
@@ -24,15 +25,15 @@ args = parser.parse_args()
 # s.view()
 
 def generate_graph():
-    paths = glob.glob("logs/log_graph_color/*.*")
+    paths = glob.glob(args.data_folder + "/*.*")
     # print(args.packers)
-    for packer in args.packers:
-        paths = glob.glob("logs/log_graph_color/colored_{}*".format(packer))
-        for path in tqdm(paths[:10]):
-            if path[-3:] == "dot":
-                s = Source.from_file(path)
-                # s.save(directory="logs/log_graph_color")
-                s.render()
+    # for packer in args.packers:
+    #     paths = glob.glob("logs/log_graph_color/colored_{}*".format(packer))
+    for path in tqdm(paths):
+        if path[-3:] == "dot":
+            s = Source.from_file(path)
+            # s.save(directory="logs/log_graph_color")
+            s.render()
 
 
 def generate_single():
