2b817758104fc0efd210cdda37f00c4f43946eb8
Pham Thanh Hung <hungthanhpham94@gmail.com>
2023-06-18 14:58:41 +0900

    add evaluation for packer identification

--- graph_based_method.py
+++ graph_based_method.py
@@ -182,19 +182,18 @@ def get_final_decision(text):
     import re
     end_of_unpacking_result = re.search(r'Final decision: ([^,]+)', text).group(1)
     packer = re.search(r'Packer: ([^,]+)', text).group(1)
+    packer_identification = re.search(r'packer_identification: ([^,]+)', text).group(1)
     file_name = re.search(r'file_name: ([^,]+)', text).group(1)
     predicted_end_of_unpacking = re.search(r'predicted-end-of-unpacking: ([^,]+)', text).group(1)
     score = re.search(r'score: ([\d.]+)', text).group(1)
-    return end_of_unpacking_result, packer, file_name, predicted_end_of_unpacking, score
+    return end_of_unpacking_result, packer, file_name, predicted_end_of_unpacking, score, packer_identification
 
 
 def evaluate():
     log_files = glob.glob(args.log_path + '/*.*')
     results = {}
-    count_sample = {}
-    count_correctd_sample = {}
-
     prediction_data = {}
+    packer_identification_data = {}
     for log_file in log_files:
         print("Processing on {}".format(log_file))
         with open(log_file, "r") as f:
@@ -205,7 +204,7 @@ def evaluate():
                     packer_name, accuracy = get_result(line)
                     results[packer_name] = accuracy
                 if "Final decision" in line:
-                    end_of_unpacking_result, packer_name, file_name, predicted_end_of_unpacking, score = get_final_decision(
+                    end_of_unpacking_result, packer_name, file_name, predicted_end_of_unpacking, score, packer_identification = get_final_decision(
                         line)
                     predicted_oep, msg = get_OEP(packer_name, file_name, predicted_end_of_unpacking)
                     if end_of_unpacking_result == "True":
@@ -216,6 +215,11 @@ def evaluate():
                         prediction_data[packer_name][file_name] = predicted_oep
                     else:
                         prediction_data[packer_name][file_name] = None
+
+                    if not (packer_name in packer_identification_data):
+                        packer_identification_data[packer_name] = []
+                    packer_identification_data[packer_name].append(packer_identification)
+
             print("avarage score is {}".format(np.mean(avg_score)))
     for packer_name, file_names in prediction_data.items():
         n_sample = len(prediction_data[packer_name])
@@ -223,10 +227,15 @@ def evaluate():
         for filename, predicted_oep in file_names.items():
             if (predicted_oep is not None) and (predicted_oep == oep_dictionary[filename]):
                 n_correct += 1
-        print("Packer: {}, end-of-unpacking accuracy: {:.3f}, OEP detection accuracy: {:.3f}, of sample: {}".format(
-            packer_name,
-            float(results[packer_name]),
-            1.0 * n_correct / n_sample, n_sample))
+        n_correct_predict_packer = sum(
+            [int(packer_name == predicted_name) for predicted_name in packer_identification_data[packer_name]])
+        print(
+            "Packer: {}, end-of-unpacking accuracy: {:.3f}, OEP detection accuracy: {:.3f}, packer_identification accuracy: {}, of sample: {}".format(
+                packer_name,
+                float(results[packer_name]),
+                1.0 * n_correct / n_sample,
+                1.0 * n_correct_predict_packer / n_sample,
+                n_sample))
 
 
 if __name__ == '__main__':
